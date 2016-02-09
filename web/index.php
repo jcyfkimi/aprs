5bcbdbdd0ef2d40d27bf66c160b29a2586e716c0
Zhang Huanjie <james@ustc.edu.cn>
2016-02-09 13:54:45 +0800

    infowindow can update now

--- web/index.php
+++ web/index.php
@@ -42,7 +42,8 @@ if(@$jiupian) {
 }
 
 function urlmessage($call,$icon, $dtmstr, $msg, $ddt) {
-	$m = "<font face=微软雅黑 size=2><img src=".$icon."> ".$call." <a href=".$_SERVER["PHP_SELF"]."?call=".$call." target=_blank>数据包</a> <a id=\\\"m\\\" href=\\\"#\\\" onclick=\\\"javascript:monitor_station('".$call."');return false;\\\">跟踪台站</a> <hr color=green>".$dtmstr."<br>";
+	$m = "<font face=微软雅黑 size=2><img src=".$icon."> ".$call." <a href=".$_SERVER["PHP_SELF"]."?call=".$call." target=_blank>数据包</a> <a id=\\\"m\\\" href=\\\"#\\\" onclick=\\\"javascript:monitor_station('".$call."');return false;\\\">";
+	$m = $m."切换跟踪台站</a> <hr color=green>".$dtmstr."<br>";
 	if( (strlen($msg)>=16) &&
 		(substr($msg,3,1)=='/') &&
 		(substr($msg,7,3)=='/A=') )      // 178/061/A=000033
@@ -152,7 +153,6 @@ if ($cmd=="tm") {
 	$r=$result->fetch_array();
 	echo "document.getElementById(\"pkts\").innerHTML = ".$r[0].";\n";
 
-
 	if (!isset($_REQUEST["call"])) 
 		exit(0);
 	$call=$_REQUEST["call"];
@@ -179,6 +179,7 @@ if ($cmd=="tm") {
                 echo "addpathpoint(".$lon.",".$lat.");\n";
         }	
 	if($pathmore==1) {
+		echo "document.getElementById(\"pathlen\").innerHTML = \"路径点\"+movepath.length;\n";
 		echo "map.panTo(new BMap.Point(".$lon.",".$lat."));\n";
 		echo "polyline = new BMap.Polyline(movepath,{strokeColor:\"blue\", strokeWeight:3, strokeOpacity:0.5});\n";
 		echo "map.addOverlay(polyline);\n";
@@ -207,13 +208,173 @@ function top_menu() {
 	$result = $mysqli->query($q);
 	$r=$result->fetch_array();
 	echo $r[0]."</div> 数据包 ";
-
-	echo " <a href=".$_SERVER["PHP_SELF"]."?about target=_blank>关于本站</a> <div id=msg></div><p>";
+	echo " <a href=".$_SERVER["PHP_SELF"]."?about target=_blank>关于本站</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div id=msg></div> <div id=pathlen></div><p>";
 }
 
-if ( ($cmd!="map") ) {
+if ($cmd=="map") {  
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<style type="text/css">
+		body, html{width: 100%;height: 100%;margin:0;font-family:"微软雅黑";}
+		#full {height:100%; width: 100%;}
+		#top {height:25px; width: 100%;}
+		#allmap {height:100%; width: 100%;}
+		#control{width:100%;}
+		#calls { display:inline} 
+		#pkts { display:inline} 
+		#msg { display:inline; color:green} 
+		#pathlen { display:inline; color:green} 
+	</style>
+	<title>APRS地图</title>
+	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=7RuEGPr12yqyg11XVR9Uz7NI"></script>
+</head>
+<body>
+<div id="full">
+	<div id="top"><?php top_menu();?></div>
+	<div id="allmap"></div>
+</div>
+</body>
+</html>
+<script type="text/javascript">
+var markers = new Array();
+var labels = new Array();
+var lasttms = new Array();
+var iconurls = new Array();
+var infowindows = new Array();
+var totalmarkers=0;
+var lastupdatetm=0;
+var movepath = new Array();
+var call="";
 
+function monitor_station(mycall) {
+	if(movepath.length>0) {
+		map.removeOverlay(polyline);
+		movepath.splice(0,movepath.length);
+		document.getElementById("pathlen").innerHTML = "";
+	}	
+	document.getElementById("msg").innerHTML = "";
+	if(call==mycall) {
+		call="";
+		return;
+	}
+	call=mycall;
+	document.getElementById("msg").innerHTML = "正在跟踪"+call;
+       	map.setZoom(15);
+}
+
+function addpathpoint(lon, lat){
+	var p = new BMap.Point(lon,lat);
+	movepath.push (p);
+}
+
+function setstation(lon, lat, label,tm, iconurl, msg)
+{	for (var i=0; i< totalmarkers; i++) {
+		if(labels[i]==label) {
+			if(tm<=lasttms[i]) return;
+			markers[i].setPosition( new BMap.Point(lon, lat) );
+			infowindows[i].setContent(msg);
+			lasttms[i] = tm;
+			if(tm>lastupdatetm) lastupdatetm = tm;
+			if(iconurls[i]!=iconurl) {
+				var nicon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 24)});
+				markers[i].setIcon(nicon);
+				iconurls[i]=iconurl;
+			}
+			markers[i].setZIndex(this.maxZindex++);
+    			markers[i].setAnimation(BMAP_ANIMATION_BOUNCE);
+			if(call==labels[i])  {  // 正在跟踪
+				setTimeout(function(){ markers[i].setAnimation(null); }, 500);
+			} else 
+				setTimeout(function(){ markers[i].setAnimation(null); }, 500);
+			return;
+		}
+	}
+	var icon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 24)});	
+	var marker = new BMap.Marker(new BMap.Point(lon,lat), {icon: icon});
+	marker.setLabel(new BMap.Label(label, {offset: new BMap.Size(20,-10)}));
+	var infowindow = new BMap.InfoWindow(msg, {width:300});
+	(function(){
+        	marker.addEventListener('click', function(){
+            	this.openInfoWindow(infowindow);
+        	});
+	})();
+	map.addOverlay(marker);
+	markers[totalmarkers]= marker;
+	labels[totalmarkers] = label;
+	lasttms[totalmarkers] = tm;
+	iconurls[totalmarkers]=iconurl;
+	infowindows[totalmarkers]=infowindow;
+	if(tm>lastupdatetm) lastupdatetm = tm;
+	totalmarkers=totalmarkers+1;
+	marker.setZIndex(this.maxZindex++);
+    	marker.setAnimation(BMAP_ANIMATION_BOUNCE);
+	setTimeout(function(){ marker.setAnimation(null); }, 500);
+}
+
+var xmlHttpRequest;     //XmlHttpRequest对象     
+function createXmlHttpRequest(){     
+        if(window.ActiveXObject){ //如果是IE  
+            return new ActiveXObject("Microsoft.XMLHTTP");     
+        }else if(window.XMLHttpRequest){ //非IE浏览器     
+            return new XMLHttpRequest();     
+        }     
+}     
+
+function UpdateStation(){     
+//	alert(lastupdatetm);
+        var url = '<?php echo "http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?tm=";?>'+lastupdatetm+"&call="+call+"&pathlen="+movepath.length;  
+        //1.创建XMLHttpRequest组建     
+        xmlHttpRequest = createXmlHttpRequest();     
+        //2.设置回调函数     
+        xmlHttpRequest.onreadystatechange = UpdateStationDisplay;
+        //3.初始化XMLHttpRequest组建     
+        xmlHttpRequest.open("post",url,true);     
+        //4.发送请求     
+        xmlHttpRequest.send(null);       
+}          
+//回调函数     
+function UpdateStationDisplay(){     
+        if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200){  
+        	var b = xmlHttpRequest.responseText;  
+		eval(b);
+       	   	setTimeout("UpdateStation();","2000");  
+            	return b;  
+        }     
+}    
+
+// 百度地图API功能
+var map = new BMap.Map("allmap");
+map.enableScrollWheelZoom();
+
+var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，仅包含平移和缩放按钮
+/*缩放控件type有四种类型:
+BMAP_NAVIGATION_CONTROL_SMALL：仅包含平移和缩放按钮；BMAP_NAVIGATION_CONTROL_PAN:仅包含平移按钮；BMAP_NAVIGATION_CONTROL_ZOOM：仅包含缩放按钮*/
+	
+//添加控件和比例尺
+map.addControl(top_left_control);        
+map.addControl(top_left_navigation);     
+map.addControl(top_right_navigation);    
+map.centerAndZoom(new BMap.Point(108.940178,34.5), 6);
+
+<?php
+        $call=@$_REQUEST["call"];
+        if($call!="")  {
+		echo "monitor_station(\"$call\");\n";
+	}
+?>
+createXmlHttpRequest();  
+setTimeout("UpdateStation();","1000");  
+</script>
+<?php
+	exit(0);
+}
+?>
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
 	<title>APRS relay server</title>
 </head>
@@ -228,7 +389,6 @@ div{ display:inline}
 <?php
 
 top_menu();
-}
 
 if ($cmd=="new") {
 	echo "<h3>最新收到的APRS数据包</h3>";
@@ -247,7 +407,6 @@ if ($cmd=="new") {
         	echo "</td></tr>\n";
 	}
 	echo "</table>\n";
-
 
 	echo "<h3>最新收到的无法解析APRS数据包</h3>";
 	$q="select tm,`call`,raw from aprspacket where tm>=curdate() and lat='' order by tm desc limit 10";
@@ -373,179 +532,6 @@ so.write("flashcontent4");
 </td>
 </tr>
 </table>
-<?php
-	exit(0);
-}
-
-if ($cmd=="map") {  
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<style type="text/css">
-		body, html{width: 100%;height: 100%;margin:0;font-family:"微软雅黑";}
-		#full {height:100%; width: 100%;}
-		#top {height:25px; width: 100%;}
-		#allmap {height:100%; width: 100%;}
-		#control{width:100%;}
-		#calls { display:inline} 
-		#pkts { display:inline} 
-		#msg { display:inline} 
-	</style>
-	<title>APRS数据包实况</title>
-	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=7RuEGPr12yqyg11XVR9Uz7NI"></script>
-</head>
-<body>
-<div id="full">
-	<div id="top"><?php top_menu();?></div>
-	<div id="allmap"></div>
-</div>
-</body>
-</html>
-<script type="text/javascript">
-
-var markers = new Array();
-var labels = new Array();
-var lasttms = new Array();
-var iconurls = new Array();
-var totalmarkers=0;
-var lastupdatetm=0;
-var movepath = new Array();
-var call="";
-
-function monitor_station(mycall) {
-//	alert(mycall);
-	if(call==mycall) return;
-	if(movepath.length>0) {
-		map.removeOverlay(polyline);
-		movepath.splice(0,movepath.length);
-	}	
-	call=mycall;
-	document.getElementById("msg").innerHTML = "正在跟踪"+call;
-       	map.setZoom(15);
-}
-
-var xmlHttpRequest;     
-//XmlHttpRequest对象     
-function createXmlHttpRequest(){     
-        if(window.ActiveXObject){ //如果是IE  
-            return new ActiveXObject("Microsoft.XMLHTTP");     
-        }else if(window.XMLHttpRequest){ //非IE浏览器     
-            return new XMLHttpRequest();     
-        }     
-}     
-
-function UpdateStation(){     
-//	alert(lastupdatetm);
-        var url = '<?php echo "http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?tm=";?>'+lastupdatetm+"&call="+call+"&pathlen="+movepath.length;  
-        //1.创建XMLHttpRequest组建     
-        xmlHttpRequest = createXmlHttpRequest();     
-             
-        //2.设置回调函数     
-        xmlHttpRequest.onreadystatechange = UpdateStationDisplay;
-             
-        //3.初始化XMLHttpRequest组建     
-        xmlHttpRequest.open("post",url,true);     
-             
-        //4.发送请求     
-        xmlHttpRequest.send(null);       
-}          
-    //回调函数     
-function UpdateStationDisplay(){     
-        if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200){  
-        	var b = xmlHttpRequest.responseText;  
-		eval(b);
-       	   	setTimeout("UpdateStation();","2000");  
-            	return b;  
-        }     
-}    
-
-function addpathpoint(lon, lat){
-	var p = new BMap.Point(lon,lat);
-	movepath.push (p);
-	
-}
-
-
-function addstation(lon, lat, label, tm, iconurl, msg)
-{
-	var icon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 24)});	
-	var marker = new BMap.Marker(new BMap.Point(lon,lat), {icon: icon  });
-	marker.setLabel(new BMap.Label(label, {offset: new BMap.Size(20,-10)}));
-	(function(){
-        	marker.addEventListener('click', function(){
-            	this.openInfoWindow(new BMap.InfoWindow(msg));
-        	});
-	})();
-	map.addOverlay(marker);
-	markers[totalmarkers]= marker;
-	labels[totalmarkers] = label;
-	lasttms[totalmarkers] = tm;
-	iconurls[totalmarkers]=iconurl;
-	if(tm>lastupdatetm) lastupdatetm = tm;
-	totalmarkers=totalmarkers+1;
-	return marker;
-}
-
-function setstation(lon, lat, label,tm, iconurl, msg)
-{	for (var i=0; i< totalmarkers; i++) {
-		if(labels[i]==label) {
-			if(tm<=lasttms[i]) return;
-			markers[i].setPosition( new BMap.Point(lon, lat) );
-			(function(){
-        		markers[i].addEventListener('click', function(){
-            			this.openInfoWindow(new BMap.InfoWindow(msg));
-        			});    
-    			})();
-			lasttms[i] = tm;
-			if(tm>lastupdatetm) lastupdatetm = tm;
-			if(iconurls[i]!=iconurl) {
-				var nicon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {  anchor: new BMap.Size(12, 24)});	
-				markers[i].setIcon(nicon);
-				iconurls[i]=iconurl;
-			}
-			markers[i].setZIndex(this.maxZindex++);
-    			markers[i].setAnimation(BMAP_ANIMATION_BOUNCE);
-			if(call==labels[i])  {  // 正在跟踪
-				setTimeout(function(){ markers[i].setAnimation(null); }, 500);
-			} else 
-			setTimeout(function(){ markers[i].setAnimation(null); }, 500);
-			return;
-		}
-	}
-	var marker = addstation(lon, lat, label, tm, iconurl, msg);
-	marker.setZIndex(this.maxZindex++);
-    	marker.setAnimation(BMAP_ANIMATION_BOUNCE);
-	setTimeout(function(){ marker.setAnimation(null); }, 500);
-}
-
-// 百度地图API功能
-var map = new BMap.Map("allmap");
-map.enableScrollWheelZoom();
-
-var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
-var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
-var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，仅包含平移和缩放按钮
-/*缩放控件type有四种类型:
-BMAP_NAVIGATION_CONTROL_SMALL：仅包含平移和缩放按钮；BMAP_NAVIGATION_CONTROL_PAN:仅包含平移按钮；BMAP_NAVIGATION_CONTROL_ZOOM：仅包含缩放按钮*/
-	
-//添加控件和比例尺
-map.addControl(top_left_control);        
-map.addControl(top_left_navigation);     
-map.addControl(top_right_navigation);    
-
-<?php
-	echo "map.centerAndZoom(new BMap.Point(108.940178,34.5), 6);\n";
-        $call=@$_REQUEST["call"];
-        if($call!="")  {
-		echo "monitor_station(\"$call\");\n";
-	}
-?>
-createXmlHttpRequest();  
-setTimeout("UpdateStation();","1000");  
-</script>
 <?php
 	exit(0);
 }
