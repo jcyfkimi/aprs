00e74ffa4737abd6751bcdd52252c8e400cc5cef
Zhang Huanjie <james@ustc.edu.cn>
2016-02-10 00:25:17 +0800

    mobile phone opt

--- web/index.php
+++ web/index.php
@@ -147,11 +147,11 @@ if ($cmd=="tm") {
 	$q="select count(distinct(`call`)) from aprspacket where tm>=curdate()";
 	$result = $mysqli->query($q);
 	$r=$result->fetch_array();
-	echo "document.getElementById(\"calls\").innerHTML = ".$r[0].";\n";
+	echo "updatecalls(".$r[0].");\n";
 	$q="select count(*) from aprspacket where tm>=curdate()";
 	$result = $mysqli->query($q);
 	$r=$result->fetch_array();
-	echo "document.getElementById(\"pkts\").innerHTML = ".$r[0].";\n";
+	echo "updatepkts(".$r[0].");\n";
 
 	if (!isset($_REQUEST["call"])) 
 		exit(0);
@@ -166,7 +166,6 @@ if ($cmd=="tm") {
         $stmt->bind_result($glat, $glon);
 
 	$pathmore=0;
-	echo "if(movepath.lenght>0) map.removeOverlay(polyline);\n";
         while($stmt->fetch()) {
                 $lat = substr($glat,0,2) + substr($glat,2,5)/60;
                 $lon = substr($glon,0,3) + substr($glon,3,5)/60;
@@ -179,7 +178,8 @@ if ($cmd=="tm") {
                 echo "addpathpoint(".$lon.",".$lat.");\n";
         }	
 	if($pathmore==1) {
-		echo "document.getElementById(\"pathlen\").innerHTML = \"路径点\"+movepath.length;\n";
+		echo "if(movepath.length>0) map.removeOverlay(polyline);\n";
+		echo "updatepathlen();\n";
 		echo "map.panTo(new BMap.Point(".$lon.",".$lat."));\n";
 		echo "polyline = new BMap.Polyline(movepath,{strokeColor:\"blue\", strokeWeight:3, strokeOpacity:0.5});\n";
 		echo "map.addOverlay(polyline);\n";
@@ -199,16 +199,8 @@ function top_menu() {
 	if($cmd=="map") $blank = " target=_blank";
 	echo "<a href=".$_SERVER["PHP_SELF"]."?new".$blank.">最新</a> <a href=".$_SERVER["PHP_SELF"]."?today".$blank.
 	">今天</a> <a href=".$_SERVER["PHP_SELF"]."?stats".$blank.">统计</a> ";
-	echo "<a href=".$_SERVER["PHP_SELF"]."?map target=_blank>地图</a> <div id=calls>";
-	$q="select count(distinct(`call`)) from aprspacket where tm>=curdate()";
-	$result = $mysqli->query($q);
-	$r=$result->fetch_array();
-	echo $r[0]."</div>呼号/<div id=pkts>";
-	$q="select count(*) from aprspacket where tm>=curdate()";
-	$result = $mysqli->query($q);
-	$r=$result->fetch_array();
-	echo $r[0]."</div>数据包 ";
-	echo " <a href=".$_SERVER["PHP_SELF"]."?about target=_blank>关于</a>&nbsp;&nbsp;<div id=msg></div> <div id=pathlen></div><p>";
+	echo "<a href=".$_SERVER["PHP_SELF"]."?map target=_blank>地图</a> <div id=calls></div>/<div id=pkts></div> ";
+	echo " <a href=".$_SERVER["PHP_SELF"]."?about target=_blank>关于</a>&nbsp;&nbsp;<div id=msg></div><div id=pathlen></div><p>";
 }
 
 if ($cmd=="map") {  
@@ -248,7 +240,33 @@ var infowindows = new Array();
 var totalmarkers=0;
 var lastupdatetm=0;
 var movepath = new Array();
+var polyline;
 var call="";
+var ismobile=0;
+
+(function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))ismobile=b})(navigator.userAgent||navigator.vendor||window.opera,1);
+
+function updatepathlen() {
+	if(ismobile)
+		document.getElementById("pathlen").innerHTML = "/"+movepath.length;
+	else
+		document.getElementById("pathlen").innerHTML = "路径点数"+movepath.length;
+}
+
+function updatecalls(calls) {
+	if(ismobile)
+		document.getElementById("calls").innerHTML = calls+"C";
+	else
+		document.getElementById("calls").innerHTML = calls+"呼号";
+}
+
+function updatepkts(pkts) {
+	if(ismobile)
+		document.getElementById("pkts").innerHTML = pkts+"P";
+	else
+		document.getElementById("pkts").innerHTML = pkts+"数据包";
+}
+
 
 function monitor_station(mycall) {
 	if(movepath.length>0) {
@@ -262,7 +280,10 @@ function monitor_station(mycall) {
 		return;
 	}
 	call=mycall;
-	document.getElementById("msg").innerHTML = "跟踪"+call;
+	if(ismobile)
+		document.getElementById("msg").innerHTML = ""+call;
+	else
+		document.getElementById("msg").innerHTML = "跟踪"+call;
        	map.setZoom(15);
 }
 
@@ -280,7 +301,7 @@ function setstation(lon, lat, label,tm, iconurl, msg)
 			lasttms[i] = tm;
 			if(tm>lastupdatetm) lastupdatetm = tm;
 			if(iconurls[i]!=iconurl) {
-				var nicon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 24)});
+				var nicon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 12)});
 				markers[i].setIcon(nicon);
 				iconurls[i]=iconurl;
 			}
@@ -293,7 +314,7 @@ function setstation(lon, lat, label,tm, iconurl, msg)
 			return;
 		}
 	}
-	var icon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 24)});	
+	var icon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 12)});	
 	var marker = new BMap.Marker(new BMap.Point(lon,lat), {icon: icon});
 	marker.setLabel(new BMap.Label(label, {offset: new BMap.Size(20,-10)}));
 	var infowindow = new BMap.InfoWindow(msg, {width:300});
@@ -317,12 +338,27 @@ function setstation(lon, lat, label,tm, iconurl, msg)
 
 var xmlHttpRequest;     //XmlHttpRequest对象     
 function createXmlHttpRequest(){     
-        if(window.ActiveXObject){ //如果是IE  
-            return new ActiveXObject("Microsoft.XMLHTTP");     
-        }else if(window.XMLHttpRequest){ //非IE浏览器     
-            return new XMLHttpRequest();     
-        }     
-        return new XMLHttpRequest();     
+	var http_request = false;
+	if (window.XMLHttpRequest) { // Mozilla, Safari,...
+            http_request = new XMLHttpRequest();
+            if (http_request.overrideMimeType) {
+                http_request.overrideMimeType('text/xml');
+            }
+        } else if (window.ActiveXObject) { // IE
+            try {
+                http_request = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    http_request = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {}
+            }
+        }
+
+        if (!http_request) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+	return http_request;
 }     
 
 function UpdateStation(){     
@@ -339,11 +375,12 @@ function UpdateStation(){
 }          
 //回调函数     
 function UpdateStationDisplay(){     
-        if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200){  
-        	var b = xmlHttpRequest.responseText;  
-		eval(b);
+        if(xmlHttpRequest.readyState == 4){
+		if(xmlHttpRequest.status == 200){  
+        		var b = xmlHttpRequest.responseText;  
+			eval(b);
+		}
        	   	setTimeout("UpdateStation();","2000");  
-            	return b;  
         }     
 }    
 
@@ -362,7 +399,6 @@ map.addControl(top_left_control);
 map.addControl(top_left_navigation);     
 map.addControl(top_right_navigation);    
 map.centerAndZoom(new BMap.Point(108.940178,34.5), 6);
-
 <?php
         $call=@$_REQUEST["call"];
         if($call!="")  {
@@ -370,7 +406,7 @@ map.centerAndZoom(new BMap.Point(108.940178,34.5), 6);
 	}
 ?>
 createXmlHttpRequest();  
-setTimeout("UpdateStation();","1000");  
+UpdateStation();  
 </script>
 <?php
 	exit(0);
