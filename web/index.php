2a4eabf89ead1e1e73035a12cd30684addb4c2ae
Zhang Huanjie <james@ustc.edu.cn>
2016-02-10 23:01:39 +0800

    only refresh in view packets

--- web/index.php
+++ web/index.php
@@ -124,8 +124,31 @@ function urlmessage($call,$icon, $dtmstr, $msg, $ddt) {
 }
 
 if ($cmd=="tm") {
+	$starttm = microtime(true);
 	$q="delete from lastpacket where tm<=curdate()";
 	$mysqli->query($q);
+	$endtm = microtime(true); $spantm = $endtm-$starttm; $startm=$endtm; echo "//".$spantm."\n";
+
+	$llon1=$_REQUEST["llon1"];	
+	$llon2=$_REQUEST["llon2"];	
+	$llat1=$_REQUEST["llat1"];	
+	$llat2=$_REQUEST["llat2"];	
+	$lon1=$_REQUEST["lon1"];	
+	$lon2=$_REQUEST["lon2"];	
+	$lat1=$_REQUEST["lat1"];	
+	$lat2=$_REQUEST["lat2"];	
+
+	if( ($llon1==$lon1) && ($llon2==$lon2) && ($llat1==$lat1) && ($llat2==$lat2)) 
+		$viewchanged=0;
+	else  {
+		$viewchanged=1;
+		$tm=0;  // get all new lastpacket
+		echo "llon1=$lon1;\n";
+		echo "llon2=$lon2;\n";
+		echo "llat1=$lat1;\n";
+		echo "llat2=$lat2;\n";
+	}
+
 	$q="select lat,lon,`call`,unix_timestamp(tm),tm,concat(`table`,symbol),msg,datatype from lastpacket where tm>=FROM_UNIXTIME(?) and lat<>'' and not lat like '0000.00%'";
 	$stmt=$mysqli->prepare($q);
         $stmt->bind_param("i",$tm);
@@ -139,12 +162,17 @@ if ($cmd=="tm") {
 			$p=$mp->getBDEncryPoint($lon,$lat);
 			$lon = $p->getX();
 			$lat = $p->getY();
+			if ( $lon < $lon1) continue;
+			if ( $lon > $lon2) continue;
+			if ( $lat < $lat1) continue;
+			if ( $lat > $lat2) continue;
 		}
 		$icon = "img/".bin2hex($dts).".png";
 		$dmsg = urlmessage($dcall, $icon, $dtmstr, $dmsg,$ddt);
 		echo "setstation(".$lon.",".$lat.",\"".$dcall."\",".$dtm.",\"".$icon."\",\n\"".$dmsg."\");\n";
 	}
 	$stmt->close();
+	$endtm = microtime(true); $spantm = $endtm-$starttm; $startm=$endtm; echo "//".$spantm."\n";
 
 	$q="select count(distinct(`call`)) from aprspacket where tm>=curdate()";
 	$result = $mysqli->query($q);
@@ -154,6 +182,7 @@ if ($cmd=="tm") {
 	$result = $mysqli->query($q);
 	$r=$result->fetch_array();
 	echo "updatepkts(".$r[0].");\n";
+	$endtm = microtime(true); $spantm = $endtm-$starttm; $startm=$endtm; echo "//".$spantm."\n";
 
 	if (!isset($_REQUEST["call"])) 
 		exit(0);
@@ -187,6 +216,7 @@ if ($cmd=="tm") {
 		echo "updatepathlen();\n";
 		echo "map.panTo(new BMap.Point(".$lon.",".$lat."));\n";
 	}
+	$endtm = microtime(true); $spantm = $endtm-$starttm; $startm=$endtm; echo "//".$spantm."\n";
 	exit(0);
 }
 
@@ -246,6 +276,10 @@ var movepath = new Array();
 var polyline;
 var call="";
 var ismobile=0;
+var llon1=0;
+var llon2=0;
+var llat1=0;
+var llat2=0;
 
 (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))ismobile=b})(navigator.userAgent||navigator.vendor||window.opera,1);
 
@@ -298,30 +332,28 @@ function addpathpoint(lon, lat){
 	}
 }
 
-function setstation(lon, lat, label,tm, iconurl, msg)
-{	for (var i=0; i< totalmarkers; i++) {
-		if(labels[i]==label) {
-			//if(tm<=lasttms[i]) return;
-			if(tm<lasttms[i]) return;
-			if(tm==lasttms[i]) tm++;  // 如果同一个站点同样的时间戳第二次出现，说明至少过去了2秒，
-						// 可以将最后时间戳+1, 这样一来，同一个站点的信息最多重复一次
-			markers[i].setPosition( new BMap.Point(lon, lat) );
-			infowindows[i].setContent(msg);
-			lasttms[i] = tm;
-			if(tm>lastupdatetm) lastupdatetm = tm;
-			if(iconurls[i]!=iconurl) {
-				var nicon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 12)});
-				markers[i].setIcon(nicon);
-				iconurls[i]=iconurl;
-			}
-			markers[i].setZIndex(this.maxZindex++);
-    			markers[i].setAnimation(BMAP_ANIMATION_BOUNCE);
-			if(call==labels[i])  {  // 正在跟踪
-				setTimeout(function(){ markers[i].setAnimation(null); }, 500);
-			} else 
-				setTimeout(function(){ markers[i].setAnimation(null); }, 500);
-			return;
+function setstation(lon, lat, label, tm, iconurl, msg)
+{	
+	var i=labels.indexOf(label);   // 很旧的浏览器可能不支持, 如IE6/7/8
+	if(i>-1) {
+		if(tm<lasttms[i]) return;
+		markers[i].setPosition( new BMap.Point(lon, lat) );
+		infowindows[i].setContent(msg);
+		if(iconurls[i]!=iconurl) {
+			var nicon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 12)});
+			markers[i].setIcon(nicon);
+			iconurls[i]=iconurl;
 		}
+		markers[i].setZIndex(this.maxZindex++);
+		if(tm==lasttms[i]) tm++;  // 如果同一个站点同样的时间戳第二次出现，说明至少过去了2秒，
+					// 可以将最后时间戳+1, 这样一来，同一个站点的信息最多重复一次
+		else {
+    			markers[i].setAnimation(BMAP_ANIMATION_BOUNCE);
+			setTimeout(function(){ markers[i].setAnimation(null); }, 500);
+		}
+		lasttms[i] = tm;
+		if(tm>lastupdatetm) lastupdatetm = tm;
+		return;
 	}
 	var icon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 12)});	
 	var marker = new BMap.Marker(new BMap.Point(lon,lat), {icon: icon});
@@ -349,30 +381,30 @@ var xmlHttpRequest;     //XmlHttpRequest对象
 function createXmlHttpRequest(){     
 	var http_request = false;
 	if (window.XMLHttpRequest) { // Mozilla, Safari,...
-            http_request = new XMLHttpRequest();
-            if (http_request.overrideMimeType) {
-                http_request.overrideMimeType('text/xml');
-            }
+		http_request = new XMLHttpRequest();
+		if (http_request.overrideMimeType) {
+			http_request.overrideMimeType('text/xml');
+		}
         } else if (window.ActiveXObject) { // IE
-            try {
-                http_request = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch (e) {
-                try {
-                    http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e) {}
-            }
+		try {
+			http_request = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+				http_request = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {}
+		}
         }
-
         if (!http_request) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
+		alert('Giving up :( Cannot create an XMLHTTP instance');
+		return false;
         }
 	return http_request;
 }     
 
 function UpdateStation(){     
 //	alert(lastupdatetm);
-        var url = '<?php echo "http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?tm=";?>'+lastupdatetm+"&call="+call+"&pathlen="+movepath.length;  
+	var b = map.getBounds();
+        var url = '<?php echo "http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?tm=";?>'+lastupdatetm+"&call="+call+"&pathlen="+movepath.length+"&llon1="+llon1+"&llon2="+llon2+"&llat1="+llat1+"&llat2="+llat2+"&lon1="+b.getSouthWest().lng+"&lat1="+b.getSouthWest().lat+"&lon2="+b.getNorthEast().lng+"&lat2="+b.getNorthEast().lat;
         //1.创建XMLHttpRequest组建     
         xmlHttpRequest = createXmlHttpRequest();     
         //2.设置回调函数     
@@ -380,8 +412,9 @@ function UpdateStation(){
         //3.初始化XMLHttpRequest组建     
         xmlHttpRequest.open("post",url,true);     
         //4.发送请求     
-        xmlHttpRequest.send(null);       
-}          
+        xmlHttpRequest.send(null);
+}
+
 //回调函数     
 function UpdateStationDisplay(){     
         if(xmlHttpRequest.readyState == 4){
