3b35613d4d11451ccc90dfbb2b8f1b8c9372421e
Zhang Huanjie <james@ustc.edu.cn>
2016-02-14 13:44:35 +0800

    delete not inview stations

--- web/index.php
+++ web/index.php
@@ -263,7 +263,7 @@ function top_menu() {
 	if($cmd=="map") $blank = " target=_blank";
 	echo "<a href=".$_SERVER["PHP_SELF"]."?new".$blank.">最新</a> <a href=".$_SERVER["PHP_SELF"]."?today".$blank.
 	">今天</a> <a href=".$_SERVER["PHP_SELF"]."?stats".$blank.">统计</a> ";
-	echo "<a href=".$_SERVER["PHP_SELF"]."?map target=_blank>地图</a> <div id=calls></div><div id=pkts></div> ";
+	echo "<a href=".$_SERVER["PHP_SELF"]."?map target=_blank>地图</a> <div id=calls></div><div id=inview></div><div id=pkts></div> ";
 	echo "<a href=".$_SERVER["PHP_SELF"]."?about target=_blank>关于</a><div id=msg></div><div id=pathlen></div><div id=autocenter></div><p>";
 }
 
@@ -281,6 +281,7 @@ if ($cmd=="map") {
 		#allmap {height:100%; width: 100%;}
 		#control{width:100%;}
 		#calls { display:inline} 
+		#inview { display:inline;} 
 		#pkts { display:inline} 
 		#msg { display:inline; color:green} 
 		#pathlen { display:inline; color:green} 
@@ -291,7 +292,13 @@ if ($cmd=="map") {
 </head>
 <body>
 <div id="full">
-	<div id="top"><?php top_menu();?></div>
+	<div id="top"><?php
+	$blank = " target=_blank";
+	echo "<a href=".$_SERVER["PHP_SELF"]."?new".$blank.">最新</a> <a href=".$_SERVER["PHP_SELF"]."?today".$blank.
+	">今天</a>";
+	echo" <div id=calls></div><div id=inview></div><div id=pkts></div> ";
+	echo "<a href=".$_SERVER["PHP_SELF"]."?about target=_blank>关于</a><div id=msg></div><div id=pathlen></div><div id=autocenter></div></div>";
+?>
 	<div id="allmap"></div>
 </div>
 </body>
@@ -314,8 +321,16 @@ var llat1=0;
 var llat2=0;
 var jiupian=1;
 var autocenter=true;
+var debug=true;
 
 (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))ismobile=b})(navigator.userAgent||navigator.vendor||window.opera,1);
+
+function updateinview() {
+	if(ismobile)
+		document.getElementById("inview").innerHTML = "("+totalmarkers+")";
+	else
+		document.getElementById("inview").innerHTML = "(显示"+totalmarkers+")";
+}
 
 function updatepathlen() {
 	if(ismobile)
@@ -380,6 +395,44 @@ function addpathpoint(lon, lat){
 	}
 }
 
+function delstation(i) {
+	map.removeOverlay(markers[i]);
+	markers[i]=markers[totalmarkers-1];
+        labels[i]=labels[totalmarkers-1];
+        lasttms[i]=lasttms[totalmarkers-1];
+        iconurls[i]=iconurls[totalmarkers-1];
+        infowindows[i]=infowindows[totalmarkers-1];
+	markers.splice(totalmarkers-1,1);
+	labels.splice(totalmarkers-1,1);
+	lasttms.splice(totalmarkers-1,1);
+	iconurls.splice(totalmarkers-1,1);
+	infowindows.splice(totalmarkers-1,1);
+	totalmarkers--;
+	updateinview();
+}
+
+function map_resize() {
+	var b = map.getBounds();
+        lon1=b.getSouthWest().lng;
+	lat1=b.getSouthWest().lat;
+	lon2=b.getNorthEast().lng;
+	lat2=b.getNorthEast().lat;
+	i=0;
+	while(i<totalmarkers) {
+		if(call==labels[i]) {
+			i++;
+			continue;
+		}
+		p = markers[i].getPosition();
+		if( (p.lng<lon1) || (p.lat<lat1) || (p.lng>lon2) || (p.lat>lat2)) {
+			delstation(i);
+			continue;
+		}
+		i++;
+	}
+	UpdateStation();  
+}
+
 function setstation(lon, lat, label, tm, iconurl, msg)
 {	
 	var i=labels.indexOf(label);   // 很旧的浏览器可能不支持, 如IE6/7/8
@@ -397,10 +450,12 @@ function setstation(lon, lat, label, tm, iconurl, msg)
 					// 可以将最后时间戳+1, 这样一来，同一个站点的信息最多重复一次
 		else {
     			markers[i].setAnimation(BMAP_ANIMATION_BOUNCE);
-			setTimeout(function(){ markers[i].setAnimation(null); }, 500);
+			m = markers[i];
+			setTimeout((function(m){m.setAnimation(null);})(m), 500);
 		}
 		lasttms[i] = tm;
 		if(tm>lastupdatetm) lastupdatetm = tm;
+		updateinview();
 		return;
 	}
 	var icon = new BMap.Icon(iconurl, new BMap.Size(24, 24), {anchor: new BMap.Size(12, 12)});	
@@ -424,7 +479,8 @@ function setstation(lon, lat, label, tm, iconurl, msg)
 	totalmarkers=totalmarkers+1;
 	marker.setZIndex(this.maxZindex++);
     	marker.setAnimation(BMAP_ANIMATION_BOUNCE);
-	setTimeout(function(){ marker.setAnimation(null); }, 500);
+	setTimeout((function(m){m.setAnimation(null);})(marker), 500);
+	updateinview();
 }
 
 var xmlHttpRequest;     //XmlHttpRequest对象     
@@ -492,6 +548,10 @@ map.addControl(top_left_control);
 map.addControl(top_left_navigation);     
 map.addControl(top_right_navigation);    
 map.centerAndZoom(new BMap.Point(108.940178,34.5), 6);
+map.addEventListener('moveend', map_resize);
+map.addEventListener('zoomend', map_resize);
+map.addEventListener('resize', map_resize);
+
 <?php
 	echo "jiupian=$jiupian;\n";
         $call=@$_REQUEST["call"];
