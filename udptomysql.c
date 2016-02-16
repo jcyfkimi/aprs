0a84b7e2053e0e989f5b4f97d13ab48a9ffc01cd
Zhang Huanjie <james@ustc.edu.cn>
2016-02-16 23:09:58 +0800

    mic-E decode

--- udptomysql.c
+++ udptomysql.c
@@ -218,7 +218,7 @@ void ToMysql(char *buf, int len)
 		return;
 	}
 
-	if( (datatype == '`') ) {    // Mic-E
+	if( (datatype == '`') || (datatype=='\'')) {    // Mic-E
 		if( strlen(p)<8 ) 
 			goto unknow_msg;
 		if( strlen(path)<6 )
