5940296789042055a5c2bb59790b5977b732b6bd
Zhang Huanjie <james@ustc.edu.cn>
2016-02-26 17:44:54 +0800

    remove dup control

--- web/index.php
+++ web/index.php
@@ -589,18 +589,11 @@ map.enableScrollWheelZoom();
 
 var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
 var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
-var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); 
-//右上角，仅包含平移和缩放按钮
-/*缩放控件type有四种类型:
-BMAP_NAVIGATION_CONTROL_SMALL：仅包含平移和缩放按钮；
-BMAP_NAVIGATION_CONTROL_PAN:仅包含平移按钮；
-BMAP_NAVIGATION_CONTROL_ZOOM：仅包含缩放按钮
-*/
 	
 //添加控件和比例尺
 map.addControl(top_left_control);        
 map.addControl(top_left_navigation);     
-map.addControl(top_right_navigation);    
+map.addControl(new BMap.MapTypeControl());
 map.centerAndZoom(new BMap.Point(108.940178,34.5), 6);
 map.addEventListener('moveend', map_resize);
 map.addEventListener('zoomend', map_resize);
